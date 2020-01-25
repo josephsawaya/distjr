c1a50514040e63a0fd13fd2a96e9265426cdebd5
ecsmaple123 <liyou2000113@gmail.com>
2020-01-25 17:52:52 -0500

    styling

--- distjr/src/App.vue
+++ distjr/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <div class="imgcontainer">
     <img alt="Vue logo" src="./assets/logo.png">
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,5 +14,8 @@ export default {
 </script>
 
 <style>
-
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+}
 </style>
--- distjr/src/components/Login.vue
+++ distjr/src/components/Login.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="main">
     <form v-on:submit.prevent="loginFunction()">
-      <label>
-        Email:
-      </label>
-      <input v-model="username" type="text">
-      <label>
-        Password
-      </label>
-      <input v-model="password" type="password">
-      <input type="submit" value="Submit">
-    </form>
+
+
+    <div class="Username">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+    </div>
+
+    <div class="password">
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required> 
+    </div>
+
+    <div class="Submit">
+      <button type="submit">Login</button>
+    
+
     <router-link :to="{ name: 'SignUp' }">Click here to sign up!</router-link>
+    </div>
+    </form>
+   
   </div>
 </template>
 
@@ -49,5 +58,47 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.Username {
+  text-align: center;
+  display:flex;
+  margin-bottom: 10px;
+}
 
+.password {
+  text-align: center;
+  display: flex;
+  margin-top: 10px;
+}
+.password label{
+  margin: auto 10px auto auto;
+}
+.Username label{
+  margin: auto 10px auto auto;
+}
+.Submit {
+  text-align: center;
+}
+
+input[type=text], input[type=password] {
+  width: 30%;
+  padding: 12px 20px;
+  margin: auto 35% auto 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 20px 40%;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
 </style>
